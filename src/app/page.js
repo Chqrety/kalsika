2245cca24701c98715e45ba142bca6aec8f1d1ac
Chqrety <fikriaverus23@gmail.com>
2024-11-25 22:25:36 +0700

    perf: optimize building

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col items-center row-start-2 gap-8 sm:items-start">
                 <Button isIconOnly className="bg-white border border-black" href="/home" as={Link}>
-                    <Image src={"images/Logo.svg"} width={500} height={500} />
+                    <Image src={"images/Logo.svg"} width={500} height={500} alt="logo" />
                 </Button>
             </main>
         </div>
